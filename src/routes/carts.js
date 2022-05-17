9d5244c88efc78cba3d4cce8619942585ed167dc
Gustavo Ferreri <ferreri.gustavo@gmail.com>
2022-05-17 20:49:32 -0300

    Se agrega nuevo navBar, se quita opcion de socketIO, se trabaja sobre la opcion de productos

--- src/routes/carts.js
+++ src/routes/carts.js
@@ -2,8 +2,8 @@ const { Router } = require ('express');
 const router = Router();
 const { allInOne, getAll, createProduct, updateProduct, deleteProduct} = require ('../controllers/task.controller')
 
-router.get('/', getAll)
-router.get('/product', (req, res)=>{
+// router.get('/', getAll)
+router.get('/', (req, res)=>{
     res.render('productWeb')
 })
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -3,11 +3,10 @@ const router = Router();
 const { allInOne, getAll, createProduct, updateProduct, deleteProduct} = require ('../controllers/task.controller')
 
 router.get('/', getAll)
-router.get('/product', (req, res)=>{
-    res.render('productWeb')
-})
+// router.get('/:id?', (req, res)=>{
+//     res.render('productWeb')
+// })
 
-// router api/productos
 // habilitado para usr y adm
 router.get('/:id?', allInOne) 
 
